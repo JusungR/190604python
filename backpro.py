cfce722f546ef798012b6bdfc488fbaa140070a3
JusungR <yhwsr92@gmail.com>
2019-06-05 12:42:33 +0900

    backpropagation example

---
+++ backpro.py
@@ -0,0 +1,65 @@
+# set some inputs
+x = -2; y = 5; z = -4
+
+# perform the forward pass
+q = x + y # q becomes 3
+f = q * z # f becomes -12
+
+# perform the backward pass (backpropagation) in reverse order:
+# first backprop through f = q * z
+dfdz = q # df/dz = q, so gradient on z becomes 3
+dfdq = z # df/dq = z, so gradient on q becomes -4
+# now backprop through q = x + y
+dfdx = 1.0 * dfdq # dq/dx = 1. And the multiplication here is the chain rule!
+dfdy = 1.0 * dfdq # dq/dy = 1
+print(dfdx)
+print(dfdy)
+
+
+import math
+w = [2,-3,-3] # assume some random weights and data
+x = [-1, -2]
+
+# forward pass
+dot = w[0]*x[0] + w[1]*x[1] + w[2]
+f = 1.0 / (1 + math.exp(-dot)) # sigmoid function
+
+# backward pass through the neuron (backpropagation)
+ddot = (1 - f) * f # gradient on dot variable, using the sigmoid gradient derivation
+dx = [w[0] * ddot, w[1] * ddot] # backprop into x
+dw = [x[0] * ddot, x[1] * ddot, 1.0 * ddot] # backprop into w
+# we're done! we have the gradients on the inputs to the circuit
+print(dx)
+print(dw)
+
+
+x = 3 # example values
+y = -4
+
+# forward pass
+sigy = 1.0 / (1 + math.exp(-y)) # sigmoid in numerator   #(1)
+num = x + sigy # numerator                               #(2)
+sigx = 1.0 / (1 + math.exp(-x)) # sigmoid in denominator #(3)
+xpy = x + y                                              #(4)
+xpysqr = xpy**2                                          #(5)
+den = sigx + xpysqr # denominator                        #(6)
+invden = 1.0 / den                                       #(7)
+f = num * invden # done!                                 #(8)
+
+
+
+import numpy as np
+# forward pass
+W = np.random.randn(5, 10)
+X = np.random.randn(10, 3)
+D = W.dot(X)
+
+print(W.shape)
+print(X.shape)
+print(D.shape)
+
+# now suppose we had the gradient on D from above in the circuit
+dD = np.random.randn(*D.shape) # same shape as D, positional arguments
+
+dW = dD.dot(X.T) #.T gives the transpose of the matrix
+dX = W.T.dot(dD)
